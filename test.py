b5f82feb28886d5acab0607687f482e1e6fd6d7a
peterpji <peterpji@hotmail.com>
2021-01-06 18:59:41 +0200

    Add tests

---
+++ test.py
@@ -0,0 +1,39 @@
+import unittest
+from unittest.mock import patch
+import os
+import sys
+from contextlib import contextmanager
+from io import StringIO
+
+from exec import main
+
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+class TestBasicFunctionality(unittest.TestCase):
+    def test_single_command(self):
+        with patch('exec.sys.argv', [os.path.join(os.path.dirname(__file__), 'exec.py'), 'test-print']):
+            main()
+
+    def test_list(self):
+        with patch('exec.sys.argv', [os.path.join(os.path.dirname(__file__), 'exec.py'), 'test-print-list']):
+            main()
+
+    def test_print_command(self):
+        with patch('exec.sys.argv', [os.path.join(os.path.dirname(__file__), 'exec.py'), 'test-print', '--print']):
+            with patch('builtins.print') as out:
+                main()
+        self.assertEqual(out.call_args_list[0][0][0], 'echo Working')
+
+
+if __name__ == "__main__":
+    unittest.main()
