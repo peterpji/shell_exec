356825b317ed0ba81fcbb80f19c7eef9ce0641e7
peterpji <peterpji@hotmail.com>
2021-02-15 15:56:30 +0200

    Add pylint

--- exec.py
+++ exec.py
@@ -42,6 +42,7 @@ def generate_commands() -> dict:
         'safety': 'safety check --full-report',
         'coverage': ['python -m coverage run --source=. -m unittest discover', 'python -m coverage report'],
         'black': 'black --line-length=150 --skip-string-normalization --exclude=logs/ ./',
+        'pylint': f'python -m pylint {FILE_DIR}',
         # Other
         'test-print': 'echo Working',
         'test-print-list': ['echo Working once', 'echo Working twice'],
