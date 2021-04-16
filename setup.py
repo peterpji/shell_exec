7c8cd632c40e00740001f97970ed786fb72822d0
peterpji <peterpji@hotmail.com>
2021-04-16 16:40:18 +0300

    Set these as optional

--- setup.py
+++ setup.py
@@ -14,15 +14,17 @@ setuptools.setup(
     version='1.0.0',
     description='Adds exec.py tool to the command line.',
     packages=setuptools.find_packages(include=['exec', 'exec.*']),
-    setup_requires=[
-        'bandit',
-        'black',
-        'coverage',
-        'flake8',
-        'isort',
-        'pre-commit',
-        'pylint',
-        'safety',
-    ],
+    extras_require={
+        'code-quality': [
+            'bandit',
+            'black',
+            'coverage',
+            'flake8',
+            'isort',
+            'pre-commit',
+            'pylint',
+            'safety',
+        ]
+    },
     entry_points={'console_scripts': ['exec=exec.main:main']},  # TODO Might need to be put in a package?
 )
