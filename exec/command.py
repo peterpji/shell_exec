377408bbf5df356d320c8291f9bc064293d3fafd
peterpji <peterpji@hotmail.com>
2021-05-03 15:32:14 +0300

    Unused import

--- exec/command.py
+++ exec/command.py
@@ -2,7 +2,6 @@ import logging
 import os
 import platform
 import subprocess
-import sys
 from multiprocessing import Process
 from types import FunctionType, MethodType
 from typing import Dict, List, Optional, Union
