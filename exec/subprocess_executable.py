0ae6a0280d705502af8ed31349e4d64e13f2108b
peterpji <peterpji@hotmail.com>
2021-05-06 09:21:11 +0300

    Avoid blocking issues by assigning each output printing task to a thread

--- exec/subprocess_executable.py
+++ exec/subprocess_executable.py
@@ -1,10 +1,30 @@
 import subprocess
 import sys
+from multiprocessing import Queue
+from threading import Thread
 from time import sleep
 from typing import Optional
 
 
+def _format_line(line, index):
+    return line if index is None else f'[{index}] {line}'
+
+
+def reader(process: subprocess.Popen, feed_type: str, queue: Queue, index: int):
+    while process.poll() is None:
+        line = getattr(process, feed_type).readline()
+        if not line:
+            sleep(0.01)
+            continue
+        line = _format_line(line.decode(), index)
+        queue.put((feed_type, line))
+
+
 class SubprocessExecutable:
+    queue: Queue
+    stdout_reader: Thread
+    stderr_reader: Thread
+
     def __init__(self, command: str, parallel: bool = False, index: Optional[int] = None, **common_kwargs) -> None:
         self.index = index
         self.content = ['', '']
@@ -16,7 +36,7 @@ class SubprocessExecutable:
                 'stdout': subprocess.PIPE,
                 'stderr': subprocess.PIPE,
             }
-        else:
+        else:  # Avoiding pipe has some benefits, e.g. printing with color to the console
             kwargs = {
                 **common_kwargs,
                 'stdin': sys.stdin,
@@ -26,40 +46,37 @@ class SubprocessExecutable:
 
         self.sub_command = subprocess.Popen(command, **kwargs)  # pylint: disable=subprocess-run-check # nosec
 
-    def _format_line(self, content):
-        return content if self.index is None else f'[{self.index}] {content}'
+    def _start_output_reader(self, feed: str):
+        output_reader = Thread(target=reader, args=[self.sub_command, feed, self.queue, self.index])
+        output_reader.start()
+        return output_reader
 
-    def print_if_content(self, source, dest, content_index):
-        new_output: str = source.read1().decode()
-        self.content[content_index] += new_output
-        if '\n' not in self.content[content_index]:
-            return
-        sections = self.content[content_index].split('\n')
-        line_to_print = self._format_line(sections[0])
-        print(line_to_print, file=dest)
-        self.content[content_index] = self.content[content_index].replace(sections[0] + '\n', '', 1)
+    def _print_output_from_queue(self):
+        if self.queue.empty():
+            sleep(0.01)
+            return False
+        feed_type, line = self.queue.get()
+        print(f"{line}", end='', file=getattr(sys, feed_type))
+        return True
 
-    def print_rest(self, source, dest, content_index):
-        new_output: str = source.read1().decode()
-        self.content[content_index] += new_output
-        if not self.content[content_index]:
-            return
-        line_to_print = self._format_line(self.content[content_index])
-        print(line_to_print, file=dest)
+    def _output_print_loop(self):
+        while self.stdout_reader.is_alive() or self.stderr_reader.is_alive() or not self.queue.empty():
+            if self._print_output_from_queue():
+                sleep(0.01)
+                continue
 
     def __call__(self) -> None:
         if not self.parallel:
             self.sub_command.wait()
             return self.sub_command.returncode
 
-        stdout_args = [self.sub_command.stdout, sys.stdout, 0]
-        stderr_args = [self.sub_command.stderr, sys.stderr, 1]
-        while self.sub_command.poll() is None:
-            sleep(0.01)
-            self.print_if_content(*stdout_args)
-            self.print_if_content(*stderr_args)
-        self.print_rest(*stdout_args)
-        self.print_rest(*stderr_args)
+        self.queue = Queue()
+        self.stdout_reader = self._start_output_reader('stdout')
+        self.stderr_reader = self._start_output_reader('stderr')
+
+        self._output_print_loop()
+
+        assert self.sub_command.poll() is not None, 'Output printing loop should not exit before the process is done'
         return self.sub_command.returncode
 
 
