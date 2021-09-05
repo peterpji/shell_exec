69856a23812401184aa177557eb81ae7d1ecd435
peterpji <peterpji@hotmail.com>
2021-09-05 10:05:51 +0300

    Refactoring

--- exec/str_sub_command/str_sub_command.py
+++ exec/str_sub_command/str_sub_command.py
@@ -4,7 +4,7 @@ import platform
 import sys
 from subprocess import PIPE, Popen
 from types import FunctionType, MethodType
-from typing import Dict, Optional, Union
+from typing import Any, Dict, Optional, Union
 
 from exec.str_sub_command.printer import ShellPrinter
 
@@ -13,7 +13,10 @@ command_low_level_type = Union[FunctionType, MethodType, str, list]
 
 def _get_patched_environ() -> Dict[str, str]:
     def win_add_ssh_to_path(env):
-        system32 = os.path.join(os.environ['SystemRoot'], 'SysNative' if platform.architecture()[0] == '32bit' else 'System32')
+        system32 = os.path.join(
+            os.environ['SystemRoot'],
+            'SysNative' if platform.architecture()[0] == '32bit' else 'System32',
+        )
         ssh_path = os.path.join(system32, 'OpenSSH')
         env['PATH'] += ';' + ssh_path
 
@@ -21,6 +24,25 @@ def _get_patched_environ() -> Dict[str, str]:
     if platform.system() == 'Windows':
         win_add_ssh_to_path(env)
     return env
+
+
+def _parse_popen_kwargs(parallel: bool) -> dict[str, Any]:
+    subprocess_kwargs = {'shell': True, 'env': _get_patched_environ()}
+
+    if parallel:
+        kwargs = {
+            **subprocess_kwargs,
+            'stdout': PIPE,
+            'stderr': PIPE,
+        }
+    else:  # Avoiding pipe has some benefits, e.g. printing with color to the console as a default on some software
+        kwargs = {
+            **subprocess_kwargs,
+            'stdin': sys.stdin,
+            'stdout': sys.stdout,
+            'stderr': sys.stderr,
+        }
+    return kwargs
 
 
 def _keyboard_interrupt_handler(callback, sub_command):
@@ -47,25 +69,10 @@ class StrSubCommand:
         self.except_return_status = except_return_status
         self.print_prefix = '' if index is None else f'[{index}] '
 
-        subprocess_kwargs = {'shell': True, 'env': _get_patched_environ()}
-
-        if parallel:
-            kwargs = {
-                **subprocess_kwargs,
-                'stdout': PIPE,
-                'stderr': PIPE,
-            }
-        else:  # Avoiding pipe has some benefits, e.g. printing with color to the console as a default on some software
-            kwargs = {
-                **subprocess_kwargs,
-                'stdin': sys.stdin,
-                'stdout': sys.stdout,
-                'stderr': sys.stderr,
-            }
-
+        kwargs = _parse_popen_kwargs(parallel)
         self.sub_command = Popen(command, **kwargs)  # pylint: disable=subprocess-run-check # nosec
 
-    def _handle_error(self, return_code):
+    def _handle_error(self, return_code: int):
         if not self.except_return_status and return_code:
             sys.tracebacklimit = 0
             raise RuntimeError(f'Process returned with status code {return_code}')
@@ -73,7 +80,9 @@ class StrSubCommand:
     def __call__(self) -> None:
         def parallel_printer():
             output_printer.start()
-            assert self.sub_command.poll() is not None, 'Output printing loop should not exit before the process is done'
+            assert (
+                self.sub_command.poll() is not None
+            ), 'Output printing loop should not exit before the process is done'
 
         if not self.parallel:
             _keyboard_interrupt_handler(self.sub_command.wait, self.sub_command)
