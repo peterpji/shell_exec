a4f3edaee98c33567d0691eb5a12cb3932aa0a70
peterpji <peterpji@hotmail.com>
2021-09-05 10:43:18 +0300

    More test refactoring

--- test.py
+++ test.py
@@ -1,66 +1,49 @@
 import os
-import sys
 import unittest
-from contextlib import contextmanager
-from io import StringIO
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
 from exec.main import main
-
-
-@contextmanager
-def captured_output():
-    new_out, new_err = StringIO(), StringIO()
-    old_out, old_err = sys.stdout, sys.stderr
-    try:
-        sys.stdout, sys.stderr = new_out, new_err
-        yield sys.stdout, sys.stderr
-    finally:
-        sys.stdout, sys.stderr = old_out, old_err
 
 
 def patch_input(inputs: list[str]):
     return patch('exec.main.sys.argv', [os.path.join(os.path.dirname(__file__), 'exec.py'), *inputs])
 
 
-def patch_popen(*args, **kwargs):
-    return patch('exec.str_sub_command.str_sub_command.Popen')(*args, **kwargs)
-
-
 class TestBasicFunctionality(unittest.TestCase):
+    def setUp(self) -> None:
+        self.mock_shell = Mock()
+        patch('exec.str_sub_command.str_sub_command.Popen', self.mock_shell).start()
+        return super().setUp()
+
     @patch_input(['test-print'])
-    @patch_popen
-    def test_single_command(self, mock_shell):
+    def test_single_command(self):
         main()
-        self.assertEqual(mock_shell.call_args_list[0][0][0], 'echo Working')
+        self.assertEqual(self.mock_shell.call_args_list[0][0][0], 'echo Working')
 
     @patch_input(['test-print', '123'])
-    @patch_popen
-    def test_single_command_with_args(self, mock_shell):
+    def test_single_command_with_args(self):
         main()
-        self.assertEqual(mock_shell.call_args_list[0][0][0], 'echo Working 123')
+        self.assertEqual(self.mock_shell.call_args_list[0][0][0], 'echo Working 123')
 
     @patch_input(['test-print-list'])
-    @patch_popen
-    def test_list(self, mock_shell):
+    def test_list(self):
         main()
 
         expectation = ['echo Working once', 'echo Working twice']
 
-        for call, expectation_elem in zip(mock_shell.call_args_list, expectation):
+        for call, expectation_elem in zip(self.mock_shell.call_args_list, expectation):
             self.assertEqual(call[0][0], expectation_elem)
-        self.assertEqual(len(mock_shell.call_args_list), len(expectation))
+        self.assertEqual(len(self.mock_shell.call_args_list), len(expectation))
 
     @patch_input(['test-print-list', '123'])
-    @patch_popen
-    def test_list_with_args(self, mock_shell):
+    def test_list_with_args(self):
         main()
 
         expectation = ['echo Working once', 'echo Working twice']
 
-        for call, expectation_elem in zip(mock_shell.call_args_list, expectation):
+        for call, expectation_elem in zip(self.mock_shell.call_args_list, expectation):
             self.assertEqual(call[0][0], expectation_elem)
-        self.assertEqual(len(mock_shell.call_args_list), len(expectation))
+        self.assertEqual(len(self.mock_shell.call_args_list), len(expectation))
 
     @patch_input(['test-print', '--print'])
     @patch('builtins.print')
