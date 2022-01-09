f89e207e18bab07bbd3e04319f6879bece938a88
peterpji <peterpji@hotmail.com>
2022-01-09 09:01:47 +0200

    Improve naming and test resilience

--- exec/tests/test_main.py
+++ exec/tests/test_main.py
@@ -7,17 +7,18 @@ from exec.tests.utils import BaseTestClass, set_commands_base, patch_input
 
 
 class TestBasicFunctionality(BaseTestClass):
-    def test_single_command_with_args(self):
+    def test_passing_args(self):
         patch_input(['test-print', '123'])
         with set_commands_base({'test-print': Command('echo Working')}):
             main()
         self.assertEqual(self.mock_shell.call_args_list[0][0][0], 'echo Working 123')
 
-    def test_list_with_args(self):
-        patch_input(['test-print-list', '123'])
-        main()
+    def test_list_ignores_args(self):
+        patch_input(['test-print', '123'])
 
         expectation = ['echo Working once', 'echo Working twice']
+        with set_commands_base({'test-print': Command(expectation)}):
+            main()
 
         for call, expectation_elem in zip(self.mock_shell.call_args_list, expectation):
             self.assertEqual(call[0][0], expectation_elem)
@@ -26,7 +27,8 @@ class TestBasicFunctionality(BaseTestClass):
     @patch('builtins.print')
     def test_print_command(self, mock_print):
         patch_input(['test-print', '--print'])
-        main()
+        with set_commands_base({'test-print': Command('echo Working')}):
+            main()
         self.assertEqual(mock_print.call_args_list[0][0][0].command, 'echo Working')
         self.assertEqual(len(self.mock_shell.call_args_list), 0)
 
