7408629fbb20af5809ca929a2d691bf06f9099ff
peterpji <peterpji@hotmail.com>
2024-09-16 18:04:26 +0300

    change tool name to something more unique

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ import setuptools
 DIR = os.path.abspath(os.path.dirname(__file__))
 
 setuptools.setup(
-    name='run',
+    name='doer',
     version='1.0.0',
-    description='Adds run.py tool to the command line.',
+    description='Adds doer.py tool to the command line.',
     packages=setuptools.find_packages(where='src', include=['*']),
     extras_require={},
-    entry_points={'console_scripts': ['run=src.main:main']},
+    entry_points={'console_scripts': ['doer=src.main:main']},
 )
